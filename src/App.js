3a8083531ef7798a80fb6141cb28d203d4202d83
trishaDas13 <trisha.das1308@gmail.com>
2024-01-09 13:16:48 +0530

    header, footer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Translator from './comps/Translator';
 import Options from './comps/Options';
+import Header from './comps/Header';
+import Footer from './comps/Footer';
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Translator/>
+      <Footer/>
       
     </div>
   );
---
+++ src/comps/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <p className='footer'>All rights reserved by <a href="https://www.linkedin.com/in/trisha-das1308/" target='_blank' style={{color: "white"}}>Trisha Das</a> 🌟 </p>
+  )
+}
+
+export default Footer
